13cdf7f3d07ee7447938f62c78fe0355e94ebea1
Mick Ryan <mick@evilbits.io>
2015-04-24 16:13:27 -0700

    Cleaning up config.js to remove dependencies

--- config.js
+++ config.js
@@ -13,19 +13,8 @@ System.config({
     "asyncly/EventEmitter2": "github:asyncly/EventEmitter2@0.4.14",
     "jquery": "github:components/jquery@2.1.3",
     "lodash": "npm:lodash@3.6.0",
-    "stencil-utils": "github:stencil-utils@0.0.3",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
-    },
-    "github:stencil-utils@0.0.2": {
-      "asyncly/EventEmitter2": "github:asyncly/EventEmitter2@0.4.14",
-      "jquery": "github:components/jquery@2.1.3",
-      "lodash": "npm:lodash@3.6.0"
-    },
-    "github:stencil-utils@0.0.3": {
-      "asyncly/EventEmitter2": "github:asyncly/EventEmitter2@0.4.14",
-      "jquery": "github:components/jquery@2.1.3",
-      "lodash": "npm:lodash@3.6.0"
     },
     "npm:lodash@3.6.0": {
       "process": "github:jspm/nodelibs-process@0.1.1"
