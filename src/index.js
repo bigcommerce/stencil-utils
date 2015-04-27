5e51a1c516ccafb516c6dbd007c548e46608575f
Mick Ryan <mick@evilbits.io>
2015-04-27 13:31:46 -0700

    Privatizing event classes and only exposing `.on` and `.off`

--- src/index.js
+++ src/index.js
@@ -3,40 +3,44 @@ import {
     CartEvents,
     CurrencySelectorEvents,
     ProductEvents
-} from './events/index';
+    } from './events/index';
 import { RemoteCountry } from './remote/index';
+
+var events = {
+    account: new AccountEvents(),
+    cart: new CartEvents(),
+    currencySelector: new CurrencySelectorEvents(),
+    product: new ProductEvents()
+};
+
+function init(events){
+    Object.keys(events).forEach((event) => {
+       events[event].emitterInit();
+    });
+}
+
+init(events);
 
 var internals = {
     events: {
-        init() {
-            Object.keys(this).forEach((key) => {
-                if ((key !== 'on') && (key !== 'init')){
-                    this[key].emitterInit();
-                }
-            });
-        },
         on(event, callback) {
             let eventType = event.split('-')[0];
 
-            if (this[eventType] === undefined) {
+            if (events[eventType] === undefined) {
                 throw new Error(eventType + ' is not a valid eventType');
             }
 
-            return this[eventType].on(event, callback);
+            return events[eventType].on(event, callback);
         },
         off(event, callback){
             let eventType = event.split('-')[0];
 
-            if (this[eventType] === undefined) {
-                throw new Error(eventType + 'is not a valid eventType');
+            if (events[eventType] === undefined) {
+                throw new Error(eventType + ' is not a valid eventType');
             }
 
-            return this[eventType].on(event, callback);
-        },
-        account: new AccountEvents(),
-        cart: new CartEvents(),
-        currencySelector: new CurrencySelectorEvents(),
-        product: new ProductEvents()
+            return events[eventType].off(event, callback);
+        }
     },
     remote: {
         country: new RemoteCountry()
