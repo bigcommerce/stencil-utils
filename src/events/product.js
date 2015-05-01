6a519919f18f45aa4e969c203a329ad195c079d1
haubc <hau.nguyen@bigcommerce.com>
2015-05-01 13:56:04 -0700

    BIG-16044 Product events stubs for quantity, social media and wishlist

--- src/events/product.js
+++ src/events/product.js
@@ -19,6 +19,18 @@ export default class ProductEvents extends BaseEvents {
             },
             '[data-product-options-change]': {
                 eventName: 'product-options-change',
+                trigger: ['click', 'change']
+            },
+            '[data-product-wishlist]': {
+                eventName: 'product-wishlist',
+                trigger: ['click']
+            },
+            '[data-product-quantity-change]': {
+                eventName: 'product-quantity-change',
+                trigger: ['click', 'change']
+            },
+            '[data-product-share]': {
+                eventName: 'product-share',
                 trigger: ['click']
             }
         }
