718fba9296d214f0103e74bac4e55799f9f11103
Chris Hegre <christopher.hegre@bigcommerce.com>
2015-06-12 15:30:19 -0700

    Form models for theme clientside validation

---
+++ src/forms.js
@@ -0,0 +1,7 @@
+import LoginForm from './forms/login';
+import CreateAccountForm from './forms/create_account';
+
+export default {
+    login: new LoginForm(),
+    create_account: new CreateAccountForm()
+};
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import hooks from './hooks';
 import api from './api';
+import forms from './forms';
 
-export {hooks, api};
+export {hooks, api, forms};
 export default {
     hooks: hooks,
-    api: api
+    api: api,
+    forms: forms
 };
