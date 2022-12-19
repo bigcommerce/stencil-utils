acbb81f80a34fa254b998151e97de113cea2db96
Jairo Panduro <jairo.panduro@bigcommerce.com>
2022-12-19 13:08:49 -0500

    feat: STRF-9847 BODL: Product Added and Removed

--- spec/api/cart.spec.js
+++ spec/api/cart.spec.js
@@ -2,15 +2,46 @@ import CartApi from '../../src/api/cart';
 import { getBODLEvents } from '../../src/bodl/helpers';
 
 describe('Cart Api Class', () => {
-    let cart; let
-        bodlEvents;
+    let cart;
+    let bodlEvents;
 
     beforeEach(() => {
         bodlEvents = getBODLEvents();
         cart = new CartApi();
 
         cart.makeRequest = jest.fn((_, _1, _2, _3, cb) => {
-            cb();
+            cb(null, {
+                data: {
+                    cart_id: '77d0405d-1d8c-41d7-b610-e05577510cb5',
+                    cart_item: {
+                        id: '2f6ee649-62f8-469a-9f70-43662025d901',
+                        hash: '1284827bbcd39b403d24df2cfd3103ab',
+                        product_id: 86,
+                        thumbnail:
+                            'https://build-bcapp-vm-2022-10-13t13-31-44z.store.bcdev/store/eusgb9764o/products/86/images/286/ablebrewingsystem4.1670518947.190.285.jpg?c=1',
+                        url: 'https://my-dev-store-949192786.store.bcdev/able-brewing-system',
+                    },
+                    channel_id: 1,
+                    currency: 'USD',
+                    cart_value: 499.5,
+                    line_items: [
+                        {
+                            product_id: 86,
+                            product_name: '[Sample] Chemex Coffeemaker 3 Cup',
+                            sku: 'CC3C',
+                            base_price: 49.5,
+                            sale_price: 49.5,
+                            retail_price: 0,
+                            purchase_price: 49.5,
+                            quantity: 1,
+                            currency: 'USD',
+                            discount: 0,
+                            brand_name: 'Bigcommerce',
+                            category_names: ['Kitchen', 'Shop All'],
+                        },
+                    ],
+                },
+            });
         });
     });
 
