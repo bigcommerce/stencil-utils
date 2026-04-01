88eddfb5123a01151b454916d038e9a453ce4469
bc-yevhenii-buliuk <yevhenii.buliuk@bigcommerce.com>
2026-04-01 17:07:44 +0300

    chore: update bodl-events to 1.20.0

--- spec/setup.js
+++ spec/setup.js
@@ -1,3 +1,3 @@
-import * as BodlEvents from '@bigcommerce-labs/bodl-events/dist/index.cjs';
+import * as BodlEvents from '@bigcommerce-labs/bodl-events/dist/1.20.0/index.cjs';
 
 global.bodlEvents = BodlEvents;
